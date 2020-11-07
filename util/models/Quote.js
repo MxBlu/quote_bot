cf1433b912b4b6f5cb6e9d54a7c3a1691537de92
Jumail Mundekkat <mundekkat@hotmail.com>
2020-11-07 13:07:45 +1100

    Work on datastore

---
+++ util/models/Quote.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
+const QuoteSchema = new mongoose.Schema({
+    seq: {
+        type: Number,
+        index: true
+    },
+    channel: {
+        type: String,
+        index: true
+    },
+    guild:  {
+        type: String,
+        index: true
+    },
+    message: String,
+    author: {
+        type: String,
+        index: true,
+    },
+    quoter: String,
+    link: String,
+    timestamp: Date
+});
+QuoteSchema.plugin(AutoIncrement, {id: 'guild_seq', inc_field: 'seq', reference_fields: ['guild']});
+QuoteSchema.statics.getBySeq = function (guild, seq) { 
+    return this.findOne({ guild, seq });
+}
+QuoteSchema.statics.deleteBySeq = function (guild, seq) { 
+    return this.deleteOne({ guild, seq });
+}
+QuoteSchema.statics.findByGuild = function (guild) { 
+    return this.find({ guild });
+}
+QuoteSchema.statics.findByChannel = function (channel) { 
+    return this.find({ channel });
+}
+QuoteSchema.statics.findByAuthor = function (author, guild) { 
+    return this.find({ author, guild });
+}
+
+module.exports = mongoose.Model('Quote', QuoteSchema);
--- util/store.js
+++ util/store.js
@@ -1,31 +1,20 @@
-const Redis         = require('ioredis');
+const mongoose  = require('mongoose');
 
-/*
-  Storage backend using Redis for persistence
-  Guilds are stored in an ephemeral fashion, since the source of truth is Discord itself
+const Quote = require('./models/Quote');
 
-  Store format:
- - <guildId>_roles: Set() [ <roleId> ]
- - <guildId>_<roleId>_name: String
- - <guildId>_<roleId>_notifChannel: String
- - <guildId>_<roleId>_titles: Set() [ <titleId> ]
- - title_<titleId>: String
-*/
-
-// Initialise with Redis credentials and logger
-module.exports = (redisHost, redisPort, logger) => {
-  // Redis client
-  const rclient = new Redis(redisPort, redisHost);
+module.exports = (mongoUrl, logger) => {
+  mongoose.connect(mongoUrl, { autoCreate: true, autoIndex: true });
   
-  // Guilds can be ephemeral
+  // Guilds and channels can be ephemeral
   var guilds = new Set();
+  var channels = {};
 
   rclient.on('error', (err) => {
-    logger.error(`Redis error: ${err}`);
+    logger.error(`MongoDB error: ${err}`);
   });
 
-  rclient.once('connect', () => {
-    logger.info('Redis connected', 1);
+  rclient.once('open', () => {
+    logger.info('MongoDB connected', 1);
   });
 
   return {
@@ -36,13 +25,75 @@ module.exports = (redisHost, redisPort, logger) => {
     },
 
     // Add all args as guilds to guild set
+    // Also create channel maps
     addGuilds: (...guildIds) => {
-      guildIds.forEach((g) => guilds.add(g));
+      guildIds.forEach((g) => {
+        guilds.add(g);
+        channels[g] = {};
+      });
+      
     },
 
     // Remove guild from guild set
+    // Also remove channel set
     removeGuild: (guildId) => {
       guilds.delete(guildId);
+      delete channels[guildId];
+    },
+
+    // Return channels map ( channelId => channelName )
+    getChannelMap: (guildId) => {
+      return channels[guildId];
+    },
+
+    // Add channel to channel map
+    addChannel: (guildId, channelId, channelName) => {
+      channels[guildId][channelId] = channelName;
+    },
+
+    // Remove channel from channel map
+    delChannel: (guildId, channelId) => {
+      delete channels[guildId][channelId];
+    },
+
+    // Get a quote with given seq number in a certain guild
+    getQuoteBySeq: async (guildId, seq) => {
+      return Quote.getBySeq(guildId, seq);
+    },
+
+    // Get all quotes in a certain guild
+    getQuotesByGuild: async (guildId) => {
+      return Quote.findByGuild(guildId);
+    },
+
+    // Get all quotes in a certain channel
+    getQuotesByChannel: async (channelId) => {
+      return Quote.findByChannel(channelId);
+    },
+
+    // Get all quotes in by a given author in a certain guild
+    getQuotesByAuthor: async (userId, guildId) => {
+      return Quote.findByAuthor(userId, guildId);
+    },
+
+    // Add a quote to the db
+    addQuote: async (guildId, channelId, authorId, quoterId,
+          message, link, timestamp) => {
+      let quote = new Quote({
+        channel: channelId,
+        guild: guildId,
+        message,
+        author: authorId,
+        quoter: quoterId,
+        link,
+        timestamp,
+      })
+      return quote.save();
+    },
+
+    // Delete a quote from the db
+    delQuote: async (guildId, seq) => {
+      return Quote.deleteBySeq(guildId, seq);
     },
 
     // Fetch roles from db for a given guild, returns set
@@ -59,51 +110,6 @@ module.exports = (redisHost, redisPort, logger) => {
     delRole: async (guildId, roleId) => {
       return rclient.srem(`${guildId}_roles`, roleId);
     },
-
-    // Get operating channel for a given role and guild
-    getNotifChannel: async (guildId, roleId) => {
-      return rclient.get(`${guildId}_${roleId}_notifChannel`);
-    },
-
-    // Set operating channel for a given role and guild
-    setNotifChannel: async (guildId, roleId, channelId) => {
-      return rclient.set(`${guildId}_${roleId}_notifChannel`, channelId);
-    },
-    
-    // Delete operating channel for a given role and guild
-    delNotifChannel: async (guildId, roleId) => {
-      return rclient.del(`${guildId}_${roleId}_notifChannel`);
-    },
-
-    // Fetch alertable titles for a given role and guild, returns set
-    getTitles: async (guildId, roleId) => {
-      return new Set(await rclient.smembers(`${guildId}_${roleId}_titles`));
-    },
-
-    // Add alertable title for a given role and guild
-    addTitle: async (guildId, roleId, titleId) => {
-      return rclient.sadd(`${guildId}_${roleId}_titles`, titleId);
-    },
-
-    // Delete alertable title for a given role and guild
-    delTitle: async (guildId, roleId, titleId) => {
-      return rclient.srem(`${guildId}_${roleId}_titles`, titleId);
-    },
-
-    // Delete all alertable titles for a given role and guild
-    clearTitles: async (guildId, roleId) => {
-      return rclient.del(`${guildId}_${roleId}_titles`);
-    },
-
-    // Fetch title name for a given title id
-    getTitleName: async (titleId) => {
-      return rclient.get(`title_${titleId}`);
-    },
-
-    // Set title name for a given title id
-    setTitleName: async (titleId, titleName) => {
-      return rclient.set(`title_${titleId}`, titleName);
-    }
 
   }
 }
