ae22855265ec014c2a7361df79fe413d8c87f74f
Jumail Mundekkat <mundekkat@hotmail.com>
2020-11-08 17:21:34 +1100

    Add comments; handle invalid id for delete

--- modules/command_handlers/quote_management.js
+++ modules/command_handlers/quote_management.js
@@ -6,12 +6,9 @@ module.exports = (discord, db, imm, logger) => {
   return {
 
     listquotesHandler: async (command) => {
-      // 0 args - all from guild
-      // 1 arg - either channel or user - all from channel/user filter by guild
-      // 2 args - start from seq value given
       let guildId = command.message.guild.id;
-      let quotes = null;
-      let scope = '';
+      let quotes = null; // Array of quotes for given criteria
+      let scope = ''; // Scope of list query
 
       switch (command.arguments.length) {
       case 0:
@@ -32,7 +29,7 @@ module.exports = (discord, db, imm, logger) => {
           start = parseInt(command.arguments[1]);
         }
 
-        // Handle if first arg is a channel name or channel mention
+        // Handle if first arg may be a channel name or channel mention
         let potentialChannel = null;
         let channelRx = command.arguments[0].match(/^<#(\d+)>$/);
         if (channelRx != null) {
@@ -42,6 +39,7 @@ module.exports = (discord, db, imm, logger) => {
               .cache.find(c => c.name === command.arguments[0]);
         }
 
+        // If criteria passes, get all quotes for given channel
         if (potentialChannel != null) {
           quotes = await db.getQuotesByChannel(potentialChannel.id)
               .where('seq').gte(start)
@@ -50,7 +48,7 @@ module.exports = (discord, db, imm, logger) => {
           break;
         }
 
-        // Handle if first arg is a username or user nickname
+        // Handle if first arg may be a username or user nickname
         let potentialUser = null;
         let userRx = command.arguments[0].match(/^<@!(\d+)>$/);
         if (userRx != null) {
@@ -60,6 +58,7 @@ module.exports = (discord, db, imm, logger) => {
             .cache.find(m => m.nickname === command.arguments[0] || m.user.username === command.arguments[0]);
         }
         
+        // If criteria passes, get all quotes for given user
         if (potentialUser != null) {
           quotes = await db.getQuotesByAuthor(potentialUser.id, guildId)
               .where('seq').gte(start)
@@ -70,10 +69,12 @@ module.exports = (discord, db, imm, logger) => {
         }
         break;
       default:
+        // If excessive arguments, send an error
         sendCmdMessage(command.message, 'Error: too many arguments', 3, logger);
         return;
       }
 
+      // If the result set is effectively empty, send a message indicating so
       if (quotes === null || quotes.length == 0) {
         sendCmdMessage(command.message, 'No quotes found', 2, logger);
         return;
@@ -114,26 +115,32 @@ module.exports = (discord, db, imm, logger) => {
           quote = await db
               .getQuoteBySeq(guildId, parseInt(command.arguments[0])).exec();
         } catch (e) {
+          // Will be thrown if argument is non-integer
           sendCmdMessage(command.message, 'Error: invalid argument', 3, logger);
           return;
         }
         break;
       default:
+        // If excessive arguments, send an error
         sendCmdMessage(command.message, 'Error: too many arguments', 3, logger);
         return;
       }
 
+      // If the quote is not found (either due to id not existing or no quotes in the db)
+      // send a message indicating so
       if (quote === null) {
         sendCmdMessage(command.message, 'No quotes found', 2, logger);
         return;
       }
 
+      // Get GuildMember objects for author and quoter
       let author = await command.message.guild.members.fetch(quote.author);
       let quoter = await command.message.guild.members.fetch(quote.quoter);
       // Get nickname or username if not available
       const author_name = author.nickname || author.user.username;
       const quoter_name = quoter.nickname || quoter.user.username;
 
+      // Re-generate quote from stored data
       const messagePreamble = `**${quote.seq}**: **${quoter_name}** quoted **${author_name}**:`;
       const avatar_url = (author.user.avatar &&
         `https://cdn.discordapp.com/avatars/${author.id}/${author.user.avatar}.png`) ||
@@ -155,8 +162,16 @@ module.exports = (discord, db, imm, logger) => {
       case 1:
         // Delete quote with given seq ID
         try {
-          quote = await db
+          // Attempt to delete quote with given id
+          let res = await db
               .delQuote(guildId, parseInt(command.arguments[0])).exec();
+          if (res.deletedCount != null && res.deletedCount > 0) {
+            sendCmdMessage(command.message, `Quote ${command.arguments[0]} deleted.`, 2, logger);
+            return;
+          } else {
+            sendCmdMessage(command.message, `Error: quote ${command.arguments[0]} doesn't exist`, 2, logger);
+            return;
+          }
         } catch (e) {
           sendCmdMessage(command.message, 'Error: invalid argument', 3, logger);
           return;
@@ -166,8 +181,6 @@ module.exports = (discord, db, imm, logger) => {
         sendCmdMessage(command.message, 'Error: incorrect argument count', 3, logger);
         return;
       }
-
-      sendCmdMessage(command.message, `Quote ${command.arguments[0]} deleted.`, 2, logger);
     }
     
   }
