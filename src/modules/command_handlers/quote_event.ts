417c935d143b65d9db8a2cd38214cebc80b51b2d
Jumail Mundekkat <mundekkat@hotmail.com>
2021-06-05 14:56:01 +1000

    Refactor QuoteEventHandler

---
+++ src/modules/command_handlers/quote_event.ts
@@ -0,0 +1,119 @@
+import { MessageEmbed, Client as DiscordClient, MessageReaction, GuildMember, Message } from "discord.js";
+import { Logger } from "../../util/logger.js";
+import { Store } from "../../util/store.js";
+
+const IMG_RX = /https?:\/\/[^\s]+\.(?:jpg|png)/i;
+
+export class QuoteEventHandler {
+  discord: DiscordClient;
+
+  logger: Logger;
+
+  constructor(discord: DiscordClient) {
+    this.discord = discord;
+    this.logger = new Logger("QuoteEventHandler");
+  }
+
+  public async messageReactionHandler(reaction: MessageReaction, user: GuildMember): Promise<void> {
+    // Dumb ass shit cause Discord.js doesn't resolve them
+    reaction = await reaction.fetch();
+    user = await reaction.message.guild.members.fetch(user.id);
+
+    this.logger.info(`Reaction with emoji ${reaction.emoji.name} detected`, 4);
+    // Handle emojis we care about
+    // Remove reaction if we're handling em
+    switch (reaction.emoji.name) {
+    case "#️⃣":
+      // Quote on hash react
+      this.quoteHandler(reaction.message, user);
+      reaction.remove();
+      break;
+    case "omegachair":
+    case "♿":
+      // Save on wheelchair react
+      this.quoteSaveHandler(reaction.message, user);
+      reaction.remove();
+      break;
+    }
+  }
+
+  private async quoteHandler(message: Message, quoter: GuildMember): Promise<void> {
+    // Properly resolve guild members from message author
+    // I think it's a discord.js issue
+    const author = await message.guild.members.fetch(message.author.id);
+
+    // Get nickname or username if not available
+    const author_name = author.nickname || author.user.username;
+    const quoter_name = quoter.nickname || quoter.user.username;  
+
+    // Create message to send
+    const messagePreamble = `**${quoter_name}** quoted **${author_name}**:`;
+    const embed = this.generateEmbed(message, author);
+
+    this.logger.info(`${quoter_name} quoted ${message.url}`, 2);
+    
+    // Send message with embed
+    message.channel.send(messagePreamble, embed);
+  }
+
+  private async quoteSaveHandler(message: Message, quoter: GuildMember): Promise<void> {
+    // Properly resolve guild members from message author
+    // I think it's a discord.js issue
+    const author = await message.guild.members.fetch(message.author.id);
+
+    // Get nickname or username if not available
+    const author_name = author.nickname || author.user.username;
+    const quoter_name = quoter.nickname || quoter.user.username;
+
+    // Create message to send
+    const messagePreamble = `**${quoter_name}** saved quote a by **${author_name}**:`;
+    const embed = this.generateEmbed(message, author);
+
+    // Store quoted message to db
+    await Store.get().addQuote(message.guild.id, message.channel.id, author.id, quoter.id,
+      embed.description, embed.image?.url, message.url, message.createdAt);
+
+    this.logger.info(`${quoter_name} saved quote ${message.url}`, 2);
+    
+    // Send message with embed
+    message.channel.send(messagePreamble, embed);
+  }
+
+  // Create a quote embed
+  private generateEmbed(message: Message, author: GuildMember): MessageEmbed {
+    // Generate avatar URL
+    const avatar_url = (author.user.avatar &&
+        `https://cdn.discordapp.com/avatars/${author.id}/${author.user.avatar}.png`) ||
+        author.user.defaultAvatarURL;
+    let content = `${message.content}\n`
+                + `[Link](${message.url})`;
+
+    // Create base embed
+    const embed = new MessageEmbed()
+        .setColor('RANDOM')
+        .setTimestamp(message.createdAt)
+        .setAuthor(author.nickname || author.user.username, avatar_url);
+
+    // If there's any images or attachments, add them to the embed
+    let imgRegex = message.content.match(IMG_RX);
+    if (imgRegex !== null) {
+      embed.setImage(imgRegex[0]);
+    }
+    message.attachments.map(a => {
+      if (embed.image === null) {
+        imgRegex = a.url.match(IMG_RX);
+        if (imgRegex !== null) {
+          embed.setImage(imgRegex[0]);
+          return;
+        }
+      }
+
+      content += "\n\n" +
+                  `**Attachment**: [${a.name}](${a.url})`;
+    });
+    
+    // Set embed content
+    embed.setDescription(content);
+    return embed;
+  }
+}
