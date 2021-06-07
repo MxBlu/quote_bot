d96e81f574592db704870816a523394b165c4b24
Jumail Mundekkat <mundekkat@hotmail.com>
2021-06-07 11:09:35 +1000

    Add ScrollableModalManager (from main)

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -2,3 +2,6 @@ import { LogLevels } from "../util/logger.js";
 
 // Default logging level
 export const DEFAULT_LOG_LEVEL = Number(process.env.LOG_LEVEL) || LogLevels.INFO3;
+
+// Default time for a modal to stay active
+export const DEFAULT_MODAL_DURATION = 120000; // 2 minutes
