93ef7a226ced48fb4b79e6fde1b590d5e1974117
Jumail Mundekkat <mundekkat@hotmail.com>
2021-12-25 17:33:42 +1100

    Fix indentations

--- src/graphql/User_resolver.ts
+++ src/graphql/User_resolver.ts
@@ -6,16 +6,16 @@ import { User, UserModel, UserMultiQuery, UserSingleQuery } from "../models/User
 @Resolver(of => User)
 export class UserResolver {
 
-    @Authorized()
-    @Query(returns => [User], { nullable: true })
-    public users(@Arg("guildId") guildId: string): UserMultiQuery {
-        return UserModel.getByGuild(guildId);
-    }
+  @Authorized()
+  @Query(returns => [User], { nullable: true })
+  public users(@Arg("guildId") guildId: string): UserMultiQuery {
+    return UserModel.getByGuild(guildId);
+  }
 
-    @Authorized()
-    @Query(returns => User, { nullable: true })
-    public user(@Arg("userId") userId: string, @Arg("guildId") guildId: string): UserSingleQuery {
-        return UserModel.getById(userId, guildId);
-    }
+  @Authorized()
+  @Query(returns => User, { nullable: true })
+  public user(@Arg("userId") userId: string, @Arg("guildId") guildId: string): UserSingleQuery {
+    return UserModel.getById(userId, guildId);
+  }
 
 }
--- src/support/discord_rest.ts
+++ src/support/discord_rest.ts
@@ -4,18 +4,18 @@ import { APIGuild, APIUser, Routes } from "discord-api-types/v9";
 // To reduce Discord REST API usage all over the place
 export class DiscordRESTHelper {
 
-    public static async user(token: string): Promise<APIUser> {
-        return await this.get(token, Routes.user()) as APIUser;
-    }
+	public static async user(token: string): Promise<APIUser> {
+		return await this.get(token, Routes.user()) as APIUser;
+	}
 
-    public static async guilds(token: string): Promise<APIGuild[]> {
-        return await this.get(token, Routes.userGuilds()) as APIGuild[];
-    }
+	public static async guilds(token: string): Promise<APIGuild[]> {
+		return await this.get(token, Routes.userGuilds()) as APIGuild[];
+	}
 
-    private static async get(token: string, route: `/${string}`): Promise<unknown> {
-        const client = new REST({ version: '9' }).setToken(token);
-        const response = await client.get(route, { authPrefix: "Bearer" });
-        return response;
-    }
+	private static async get(token: string, route: `/${string}`): Promise<unknown> {
+		const client = new REST({ version: '9' }).setToken(token);
+		const response = await client.get(route, { authPrefix: "Bearer" });
+		return response;
+	}
 
 }
